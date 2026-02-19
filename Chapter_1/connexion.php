bedbcfb99eca258bb5e9ed1d2d5d483808ea88ad
itsmeoussama <bekkali.oussama.solicode@gmail.com>
2026-02-19 10:52:34 +0000

    update

--- Chapter_1/connexion.php
+++ Chapter_1/connexion.php
@@ -1,5 +1,5 @@
 <?php
-require_once "info.php";
+require_once "C:\Users\Solicode\Desktop\Repo\connextion\info.php";
 
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
--- Chapter_1/info.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host = '127.0.0.1;port=3307';
-$dbname = 'blogdb';
-$username = 'root';
-$password = '';
